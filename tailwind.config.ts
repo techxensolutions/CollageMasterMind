a64e0ea248fcc2ee37aae87c49dc38c9676bc9c6
Ahmad Ali <ahmadali2882@yahoo.com>
2024-07-11 00:33:07 +0500

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4B66EA;',
+        secondary: '#447EF7'
+      }
+
+    },
+  },
+  plugins: [],
+};
+export default config;
